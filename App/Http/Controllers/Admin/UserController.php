31fb3d9e5b500ff6baef6e465ff359b414285ae9
martin Bamber <mbamber1986@gmail.com>
2021-02-23 11:16:18 +0000

    Amending folder Strucutre

--- App/Http/Controllers/Admin/UserController.php
+++ App/Http/Controllers/Admin/UserController.php
@@ -1,18 +1,9 @@
 <?php
 
 
-namespace App\Http\Controllers\Admin;
-
-
-use App\Http\Functions\BladeEngine;
-use App\Http\Models\User;
+namespace App\Http\Controllers\Ad
 
 class UserController
 {
-    public function index()
-    {
-        $users = User::all();
-        echo BladeEngine::View("Pages.Admin.Users.index",["users"=>$users]);
-    }
 
 }
