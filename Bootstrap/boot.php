ea6b8571e3086a6fda493e90ccf519ead1e1cfd6
martin Bamber <mbamber1986@gmail.com>
2021-03-14 20:24:09 +0000

    Updated the file

--- Bootstrap/boot.php
+++ Bootstrap/boot.php
@@ -1,6 +1,6 @@
 <?php
 require_once ROOT.'/vendor/autoload.php';
-//Load env files first
+//Load env files first these are important*/
 use Illuminate\Database\Capsule\Manager as Capsule;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
