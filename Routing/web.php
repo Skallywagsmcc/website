8ffc044bd35580d741568c665b7aac858e77ecad
martin Bamber <mbamber1986@gmail.com>
2021-03-15 01:00:12 +0000

    Re-added the web.php file into Routing directory

---
+++ Routing/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PasswordController;
+use App\Http\Controllers\Profile\DisplayController;
+use App\Http\Controllers\UserController;
+use App\Http\Libraries\SqlInstaller;
+use MiladRahimi\PhpRouter\Exceptions\RouteNotFoundException;
+use MiladRahimi\PhpRouter\Router;
+
+//Instantiate
+
+$router = Router::create();
+// Index controller
+$router->get("/", [UserController::class, 'index']);
+$router->get("/articles", [\App\Http\Controllers\ArticlesController::class, 'index']);
+$router->get("/articles/view/{slug}", [\App\Http\Controllers\ArticlesController::class, 'view']);
+$router->get("/", [UserController::class, 'index']);
+
+
+
+try {
+    $router->dispatch();
+} catch (RouteNotFoundException $e) {
+    // If compiler is here, it means user  wants a page that does not exist
+    // Show your 404 page or use something like this:
+    $router->getPublisher()->publish("Page not found");
+}
