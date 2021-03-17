2472505dd92dce317126d39499f2b4c1f8b19c33
martin Bamber <mbamber1986@gmail.com>
2021-03-17 00:49:14 +0000

    updated notes for personal use to show plan of action

--- notes.php
+++ notes.php
@@ -1,6 +1,17 @@
 The To Do List
 
-Manage Profile section
-Move Admin panek from /admin/ -> {{username}}/tools/moderator
-profile manager will be moved to /{{username}}/tools/profile
-account settings will be moved to /{{username}}/tools/account
+Add Gallery to profile this will be shared with the homepage featured photos if they choose to do so.
+Add comments section to images Database table will be called Gallery_Comments
+Add Likes section to profile.
+
+profile will be pulled via the link
+
+domain/username
+
+/gallery
+/Account -> requires authentication
+/settings -> required authentication
+
+
+Need to fix 2fa and get it back up and running
+
