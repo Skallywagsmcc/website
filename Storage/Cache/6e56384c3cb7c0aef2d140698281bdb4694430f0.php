f642c5420d39bfdedeb8d732ae84a11d2976cc39
martin Bamber <mbamber1986@gmail.com>
2021-04-03 00:29:52 +0100

    implemented TFA Email and Modified a few Related files

---
+++ Storage/Cache/6e56384c3cb7c0aef2d140698281bdb4694430f0.php
@@ -0,0 +1,26 @@
+
+
+
+<?php $__env->startSection("content"); ?>
+    <div class="row p-1">
+        <div class="col-md-3 col-sm-12">
+            <?php echo $__env->make("Includes.AccountNav", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        </div>
+        <div class="col-sm-12 col-md-9">
+            <div class="row p-1">
+                <div class="col head">
+                    Upload a profile picture
+                </div>
+
+                <form action="<?php echo e($url->make("account.picture.store")); ?>" method="post" enctype="multipart/form-data">
+                    <input type="file" name="upload">
+                    <button>Upload</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make("Layouts.main", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/public_html/Views/Pages/Frontend/Account/Picture.blade.php ENDPATH**/ ?>
---
+++ Storage/Cache/ac17c67ca011987b960f2b457d142db294f489b6.php
@@ -0,0 +1,10 @@
+
+
+Hello <?php echo e($user->Profile->first_name); ?>}<br>
+here is your Two Factor Authentication code <?php echo e($tfa->code); ?>
+
+
+
+
+Skallywags &copy;
+<?php echo $__env->make("Layouts.main", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/public_html/Views/Emails/Tfa.blade.php ENDPATH**/ ?>
