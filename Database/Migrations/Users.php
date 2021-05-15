95b2c801d2d952f9bbcf507324191f2f9d1ccafd
martin Bamber <mbamber1986@gmail.com>
2021-05-16 00:15:39 +0100

    Added Basic Migration Manager

---
+++ Database/Migrations/Users.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Migrations;
+
+
+use App\Http\Models\Profile;
+use App\Http\Models\User;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Users
+{
+
+    public static function up()
+    {
+        Capsule::schema()->create("users", function ($table) {
+            $table->id();
+            $table->string("username");
+            $table->string("email");
+            $table->string("password");
+            $table->integer("is_admin");
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+    public static function down()
+    {
+
+    }
+
+}
