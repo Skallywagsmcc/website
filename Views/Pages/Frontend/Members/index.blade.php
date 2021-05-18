1818018227935d867d175824c6d14382c2c64f54
martin bamber <mbamber1986@gmail.com>
2021-05-18 23:56:46 +0100

    Updated members home Section

--- Views/Pages/Frontend/Members/index.blade.php
+++ Views/Pages/Frontend/Members/index.blade.php
@@ -8,6 +8,7 @@
     {{\App\Http\Libraries\Authentication\Auth::id()}}
     {{$members->count()}}
  @foreach($members as $member)
+     <img src="/img/uploads/{{$member->user->Profile->image->image_name}}"  height="200" width="200" alt="">
 {{$member->user->Profile->user->username}}
     @endforeach
 @endsection
