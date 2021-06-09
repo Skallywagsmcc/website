a3e7c046a472e33f0ae6c64f7f0142dbd9c194d8
martin Bamber <mbamber1986@gmail.com>
2021-06-09 23:42:06 +0100

    updating layout files

---
+++ Storage/Cache/a3e7dc68a08fb719de50e0b9a78fb64613013aba.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Skallywags MCC <?php echo $__env->yieldContent("title"); ?> </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/custom/base.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    <script src="/js/functions.js" type="text/javascript"></script>
+
+
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+</head>
+<body>
+<div class="container-wrapper">
+    <?php echo $__env->make("Includes.Navbar", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <div class="container-fluid m-0 p-0">
+        <div class=" d-none d-md-block logo_wrapper pb-3 pt-3 text-center">
+            <img src="/img/logo.png" alt="Logo">
+        </div>
+        <div class=" container-fluid breadcrumbs my-2">
+            <?php echo breadcrumbs(' > '); ?>
+
+        </div>
+            <?php echo $__env->yieldContent("content"); ?>
+
+
+    </div>
+</div>
+
+<div class="footer">
+  <?php echo e($_SERVER['APP_NAME']); ?> <?php echo e(date("Y")); ?>  &copy; :  <?php echo e($_SERVER['VERSION']); ?>
+
+</div>
+</body>
+</html><?php /**PATH /var/www/html/public_html/Views/Layouts/main.blade.php ENDPATH**/ ?>
