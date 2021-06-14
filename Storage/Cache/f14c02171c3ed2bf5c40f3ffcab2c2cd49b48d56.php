3076837a51ec502e73e8cc97fb9119ac1bbc411f
martin Bamber <mbamber1986@gmail.com>
2021-06-14 23:31:24 +0000

    Updating Login layout

---
+++ Storage/Cache/f14c02171c3ed2bf5c40f3ffcab2c2cd49b48d56.php
@@ -0,0 +1,10 @@
+
+
+<?php $__env->startSection("title"); ?>
+    Page not found
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection("content"); ?>
+    Sorry the page you are looking for does not exist
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make("Layouts.Auth", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/public_html/Views/Templates/PageNotFound.blade.php ENDPATH**/ ?>
