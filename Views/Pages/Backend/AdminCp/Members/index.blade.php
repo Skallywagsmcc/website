3eadd7fc554ac4d24097733cae9cf5b1458c0098
martin Bamber <mbamber1986@gmail.com>
2021-07-25 00:23:55 +0000

    updating files

--- Views/Pages/Backend/AdminCp/Members/index.blade.php
+++ Views/Pages/Backend/AdminCp/Members/index.blade.php
@@ -1,1 +1,40 @@
-@extend
+@extends("Layouts.backend")
+
+@section("title")
+    Admin Panel : Members Home
+@endsection
+
+@section("content")
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-9 text-center">
+
+                    We have found {{$members->count()}} Registered Crew
+                    @if(($members->count() > 1) || $members->count() == 0)
+                        Members
+                    @else
+                        Member
+                    @endif
+
+
+            </div>
+            <div class="col-sm-12 col-md-3 text-center text-md-right">
+                <a href="#">Create New Member</a>
+            </div>
+        </div>
+    </div>
+{{--    list all members --}}
+    <div class="container">
+        <div class="row">
+            @foreach($members as $member)
+            <div class="col-sm-12 col-md-4">
+
+                <img src="/img/uploads/{{$member->user->Profile->image->name}}" alt="{{$member->user->username}}">
+            </div>
+            @endforeach
+        </div>
+
+    </div>
+
+@endsection
