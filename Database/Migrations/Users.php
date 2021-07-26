fc3213174134aaa8c0c70a50e5fda6b36cecceb7
martin bamber <mbamber1986@gmail.com>
2021-07-26 08:44:07 +0100

    Added Users Migration Back

---
+++ Database/Migrations/Users.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Migrations;
+
+
+use App\Http\Models\Profile;
+use App\Http\Models\User;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Users
+{
+
+    public static function up()
+    {
+        Capsule::schema()->create("users", function ($table) {
+            $table->id();
+            $table->string("username");
+            $table->string("email");
+            $table->string("password");
+            $table->integer("is_admin")->default(0);
+            $table->string("exchange_key")->nullable();
+            $table->integer("disable")->default(0);
+            $table->integer("deactivate")->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public static function down()
+    {
+
+    }
+
+}
