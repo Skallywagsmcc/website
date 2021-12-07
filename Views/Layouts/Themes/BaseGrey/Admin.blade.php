cb4a3467ffde55337d08c4e7e8e692c2ca3fc87f
mbamber1986 <mbamber1986@gmail.com>
2021-12-07 00:04:27 +0000

    Improved Layout css files on Admin Panel

--- Views/Layouts/Themes/BaseGrey/Admin.blade.php
+++ Views/Layouts/Themes/BaseGrey/Admin.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Skallywags MCC @yield("title") </title>
+    <title>Skallywags MCC @yield("title")</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/Assets/css/bootstrap.css">
     <link rel="stylesheet" href="/Assets/themes/BBB/backend/base.css"/>
@@ -14,20 +14,25 @@
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 </head>
 <body>
-<div class="container-fluid d-block py-3" id="topbar">
+<div class="container-fluid" id="topbar">
     <div class="row">
-        <div class="col-sm-12">Hello</div>
+        <div class="ml-2 mr-auto py-1">
+            <img src="/img/logo.png" class="my-2" width="50px" height="50px" alt=""> | Admin Panel
+        </div>
+        <div class=" ml-auto mr-2 d-block d-lg-none">
+            <div class="col-sm-12 py-3">
+                <a href="#" class=" menu px-3 py-2 mx-1">Menu</a>
+                <a href="#" class="closemenu px-3 py-2 mx-1">Close Menu</a>
+            </div>
+
+        </div>
     </div>
 </div>
 <div class="wrap">
     <div id="container" class="container-fluid">
         <div class="row">
-            <div class="col-sm-12 px-0 col-md-4 col-lg-2 d-none d-lg-block" id="navbar">
+            <div class="col-sm-12 px-0 mx-0 col-md-4 col-lg-2 d-none d-lg-block" id="navbar">
                 <div class="row">
-                    <div class="col-sm-12"><img src="/img/logo.png" class="img-fluid p-1" alt=""></div>
-                </div>
-                <div class="row my-1">
-
                     <div class="col-sm-12"><a href="" class="d-block py-2 active">Link a</a></div>
                     <div class="col-sm-12"><a href="" class="d-block py-2">Link B</a></div>
                     <div class="col-sm-12"><a href="" class="d-block py-2">Link c</a></div>
@@ -45,12 +50,31 @@
                 </div>
             </div>
         </div>
-
     </div>
 </div>
-<div class="footer">
+<div class="footer py-2">
     The footer goes here
 </div>
 
 </body>
 </html>
+
+
+<script>
+    $(document).ready(function () {
+        $(".closemenu").hide();
+        $(".menu").click(function () {
+            $(".closemenu").show()
+            $("#navbar").removeClass("d-none");
+            $(this).hide();
+            return false
+        })
+
+        $("body").on("click", ".closemenu", function () {
+            $(".menu").show();
+            $("#navbar").addClass("d-none");
+            $(this).hide();
+            return false;
+        })
+    })
+</script>
